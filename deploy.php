c155be60edba49e912a4fc8349b8d950696eb16c
Timon de Groot <timon.degroot@hypernode.com>
2022-08-30 17:03:28 +0200

    Update to Hypernode deploy v2
    Please note: this breaks the staging build and deploy, we need a
    separate deploy.php file for that.

--- deploy.php
+++ deploy.php
@@ -2,15 +2,14 @@
 
 namespace HipexDeployConfiguration;
 
+use HipexDeployConfiguration\ServerRole;
 use HipexDeployConfiguration\AfterDeployTask\SlackWebhook;
 
 \Deployer\set('bin/composer', '/usr/local/bin/composer2');
 \Deployer\set('default_timeout', 3600);
 \Deployer\set('keep_releases', 1);
 $configuration = new ApplicationTemplate\Magento2(
-    'git@github.com:ByteInternet/magento2.komkommer.store.git',
     ['en_US'],
-    ['en_US']
 );
 
 $configuration->setPhpVersion('php81'); // @NOTE(timon): is this ok? Normally this would be php74 or something but that doesn't apply for Hypernode.
@@ -19,8 +18,8 @@ $stagingStage = $configuration->addStage('staging', 'staging.magento2.komkommer.
 $stagingStage->addServer('production1135-hypernode.hipex.io');
 
 $productionStage = $configuration->addStage('production', 'magento2.komkommer.store', 'app');
-$productionStage->addServer('hntestgroot.hypernode.io');
-$configuration->addAfterDeployTask(new SlackWebhook());
+$productionStage->addServer('hntestgroot.hypernode.io', [ServerRole::APPLICATION]);
+//$configuration->addAfterDeployTask(new SlackWebhook());
 
 $configuration->setSharedFiles([
     'app/etc/env.php',
