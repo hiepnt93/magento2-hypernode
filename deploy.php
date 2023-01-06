4711fb9548af24a12b0a107548a02da4312e1992
Timon de Groot <timon.degroot@hypernode.com>
2023-01-06 12:33:13 +0100

    Add Brancher deployment

--- deploy.php
+++ deploy.php
@@ -1,19 +1,25 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Hypernode\DeployConfiguration;
 
-\Deployer\set('bin/composer', '/usr/local/bin/composer2');
-\Deployer\set('default_timeout', 3600);
-\Deployer\set('keep_releases', 1);
-$configuration = new ApplicationTemplate\Magento2(['en_US']);
+use Hypernode\DeployConfiguration\PlatformConfiguration\HypernodeSettingConfiguration;
 
-$configuration->setPhpVersion('php81'); // @NOTE(timon): is this ok? Normally this would be php74 or something but that doesn't apply for Hypernode.
+$configuration = new ApplicationTemplate\Magento2(['en_US']);
+$configuration->addPlatformConfiguration(
+    new HypernodeSettingConfiguration('php_version', '8.1')
+);
 
 $stagingStage = $configuration->addStage('staging', 'staging.magento2.komkommer.store', 'hypernode');
 $stagingStage->addServer('production1135-hypernode.hipex.io');
 
-$productionStage = $configuration->addStage('production', 'magento2.komkommer.store', 'app');
+$productionStage = $configuration->addStage('production', 'magento2.komkommer.store');
 $productionStage->addServer('hntestgroot.hypernode.io');
+
+$acceptanceStage = $configuration->addStage('acceptance', 'acceptance.komkommer.store');
+$acceptanceStage->addBrancherServer('hntestgroot')
+    ->setLabels(['stage=acceptance', 'ci_ref=' . (\getenv('GITHUB_HEAD_REF') ?: 'none')]);
 
 $configuration->setSharedFiles([
     'app/etc/env.php',
